d3086908c60b8947eabbc5a3791b5098d5c38c82
naylasyitha <naylasyitha30@gmail.com>
2026-05-27 22:59:33 +0700

    ...

--- bootstrap/bootstrap.go
+++ bootstrap/bootstrap.go
@@ -58,7 +58,7 @@ func Start() error {
 	}
 
 	app := fiber.New()
-	app.Static("/assets", "./assets")
+	app.Static("/assets", "/app/assets")
 	app.Use(cors.New(cors.Config{
 		AllowOrigins: "*",
 		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
